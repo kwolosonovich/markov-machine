9f62767a0412c35746cc7c2c72df00838faa0d0e
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-20 12:51:22 -0700

    added axios

--- makeText.js
+++ makeText.js
@@ -1,2 +1,14 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
+
+
+// read file
+const readFile = (file) => {
+    fs.readFile(file, 'utf8', function(err, data) {
+        
+    });
+}
