5ec72a5667305d0444ba431d8dccc842eecfdde0
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-08-20 12:41:00 -0700

    finished markov tests

--- markov.test.js
+++ markov.test.js
@@ -1,15 +1,13 @@
 const { timeStamp } = require("console")
 const { MarkovMachine } = require("./markov")
 
-let mm = null
-
 describe("class MarkovMachine", () => {
   // beforeEach(() => {
   //   mm = new MarkovMachine("the cat in");
   // });
 
   test("make chains", () => {
-    mm = new MarkovMachine("the cat in");
+    let mm = new MarkovMachine("the cat in");
 
     expect(mm.chains).toEqual({
       the: ["cat"],
@@ -19,35 +17,13 @@ describe("class MarkovMachine", () => {
   });
 
   test('get random', () => {
-    // mm = new MarkovMachine("the cat in");
-
-    // mm == MarkovMachine();
-     mm == new MarkovMachine();
-    expect("the cat").toContain(MarkovMachine.random(["cat"])); 
-       
+    let mm = new MarkovMachine('the cat');
+    expect(["the", "cat"]).toContain(mm.random(["the", "cat"]));    
   });
 
-  // test('')
-
-  // test('make text', () => {
-  //       // mm = new MarkovMachine("the cat in");
-
-  // })
-
+  test('make text', () => {
+    let text = []
+    text.push(new MarkovMachine("the", "cat"));
+    expect(text.length).toBe(1)
+  })
 });
-
-
-
-
-    //   let keyArr = []
-    //   keyArr.push(mm.random)
-    //   expect(keyArr).toEqual(expect.arrayContaining(expected))
-
-    // expect([mm.randomKey]).toEqual(expect.arrayContaining(expected)); 
-    // const expected = 'the cat in the hat'
-    // console.log(expected)
-    // console.log(mm.random)
-    // expect(mm.randomKey).toEqual(expect.stringContaining(expected))   
-    // expect(expected).toEqual(expect.stringContaining(mm.randomKey)); 
-    // expect(mm.random).toContain(['the', 'cat']);   
-    // expect([the, cat]).toContain(mm.random([the, cat]));  
